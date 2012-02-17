5d3b9eda07fe5ead362bc58bcd887bc3d393b219
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-02-17 11:02:20 -0600

    updates

--- lib/task/generator/sfGenerateTestTask.class.php
+++ lib/task/generator/sfGenerateTestTask.class.php
@@ -8,11 +8,7 @@ require_once dirname(__FILE__).'/sfTaskExtraGeneratorBaseTask.class.php';
  * @package     sfTaskExtraPlugin
  * @subpackage  task
  * @author      Kris Wallsmith <kris.wallsmith@symfony-project.com>
-<<<<<<< HEAD
  * @version     SVN: $Id: sfGenerateTestTask.class.php 24538 2009-11-30 06:25:42Z dwhittle $
-=======
- * @version     SVN: $Id: sfGenerateTestTask.class.php 26469 2010-01-11 06:53:59Z Kris.Wallsmith $
->>>>>>> ba8708782531e237c25c55f198c7eacc5feee572
  */
 class sfGenerateTestTask extends sfTaskExtraGeneratorBaseTask
 {
@@ -26,13 +22,10 @@ class sfGenerateTestTask extends sfTaskExtraGeneratorBaseTask
     ));
 
     $this->addOptions(array(
-<<<<<<< HEAD
       new sfCommandOption('disable-defaults', null, sfCommandOption::PARAMETER_NONE, 'Disables detection of generator options'),
       new sfCommandOption('template', null, sfCommandOption::PARAMETER_REQUIRED, 'The unit test template to use'),
       new sfCommandOption('database', null, sfCommandOption::PARAMETER_NONE, 'Include database initialization'),
       new sfCommandOption('without-methods', null, sfCommandOption::PARAMETER_NONE, 'Omit class method stubs'),
-=======
->>>>>>> ba8708782531e237c25c55f198c7eacc5feee572
       new sfCommandOption('force', null, sfCommandOption::PARAMETER_NONE, 'Overwrite any existing test file'),
       new sfCommandOption('editor-cmd', null, sfCommandOption::PARAMETER_REQUIRED, 'Open script with this command upon creation'),
     ));
@@ -62,7 +55,6 @@ EOF;
   {
     if (!class_exists($arguments['class']))
     {
-<<<<<<< HEAD
       throw new InvalidArgumentException(sprintf('The class "%s" could not be found.', $arguments['class']));
     }
 
@@ -137,107 +129,10 @@ EOF;
 
 
 EOF;
-=======
-      throw new InvalidArgumentException(sprintf('The class "%s" does not exist.', $arguments['class']));
-    }
-
-    // base lib and test directories
-    $r = new ReflectionClass($arguments['class']);
-    list($libDir, $testDir) = $this->getDirectories($r->getFilename());
-    $path = str_replace($libDir, '', dirname($r->getFilename()));
-    $test = $testDir.'/unit'.$path.'/'.$r->getName().'Test.php';
-
-    // use either the test directory or project's bootstrap
-    if (!file_exists($bootstrap = $testDir.'/bootstrap/unit.php'))
-    {
-      $bootstrap = sfConfig::get('sf_test_dir').'/bootstrap/unit.php';
-    }
-
-    if (file_exists($test) && $options['force'])
-    {
-      $this->getFilesystem()->remove($test);
-    }
-
-    if (file_exists($test))
-    {
-      $this->logSection('task', sprintf('A test script for the class "%s" already exists.', $r->getName()), null, 'ERROR');
-    }
-    else
-    {
-      $this->getFilesystem()->copy(dirname(__FILE__).'/skeleton/test/UnitTest.php', $test);
-      $this->getFilesystem()->replaceTokens($test, '##', '##', array(
-        'CLASS'     => $r->getName(),
-        'BOOTSTRAP' => $this->getBootstrapPathPhp($bootstrap, $test),
-        'DATABASE'  => $this->isDatabaseClass($r) ? "\n\$databaseManager = new sfDatabaseManager(\$configuration);\n" : '',
-      ));
-    }
-
-    if (isset($options['editor-cmd']))
-    {
-      $this->getFilesystem()->execute($options['editor-cmd'].' '.escapeshellarg($test));
-    }
-  }
-
-  /**
-   * Returns reference of the bootstrap file from the test file.
-   * 
-   * @param string $bootstrapFile
-   * @param string $testFile
-   * 
-   * @return string PHP code for referencing the bootstrap file from the test file
-   */
-  protected function getBootstrapPathPhp($bootstrapFile, $testFile)
-  {
-    if (0 === strpos($testFile, $path = realpath(dirname($bootstrapFile).'/..')))
-    {
-      $path = str_repeat('/..', substr_count(dirname(str_replace($path, '', $testFile)), DIRECTORY_SEPARATOR));
-    }
-    else if (0 === strpos($bootstrapFile, sfConfig::get('sf_test_dir')) && 0 === strpos($testFile, sfConfig::get('sf_root_dir')))
-    {
-      $path = str_repeat('/..', substr_count(dirname(str_replace(sfConfig::get('sf_root_dir'), '', $testFile)), DIRECTORY_SEPARATOR)).'/test';
-    }
-    else
-    {
-      throw new InvalidArgumentException(sprintf('A relative path from "%s" to "%s" could not be determined.', $testFile, $bootstrapFile));
-    }
-
-    return sprintf('dirname(__FILE__).\'%s/bootstrap/unit.php\'', $path);
-  }
-
-  /**
-   * Returns paths the lib and test directory corresponding to the supplied file path.
-   * 
-   * @param string $path An absolute path
-   * 
-   * @return array The supplied path's lib and test directories
-   * 
-   * @throws InvalidArgumentException If the path is not in the project of any connected plugins' lib directories
-   */
-  protected function getDirectories($path)
-  {
-    if (0 === strpos($path, sfConfig::get('sf_lib_dir')))
-    {
-      return array(sfConfig::get('sf_lib_dir'), sfConfig::get('sf_test_dir'));
-    }
-    else
-    {
-      foreach ($this->configuration->getPluginSubPaths('/lib') as $pluginLibDir)
-      {
-        if (0 === strpos($path, $pluginLibDir))
-        {
-          // create the test directory before normalizing its path
-          if (!file_exists($testDir = $pluginLibDir.'/../test'))
-          {
-            $this->getFilesystem()->mkdirs($testDir);
-          }
-
-          return array($pluginLibDir, realpath($testDir));
->>>>>>> ba8708782531e237c25c55f198c7eacc5feee572
         }
       }
     }
 
-<<<<<<< HEAD
     $this->getFilesystem()->copy(dirname(__FILE__).sprintf('/skeleton/test/UnitTest%s.php', $template), $test);
     $this->getFilesystem()->replaceTokens($test, '##', '##', array(
       'CLASS'    => $r->getName(),
@@ -250,27 +145,5 @@ EOF;
     {
       $this->getFilesystem()->execute($options['editor-cmd'].' '.$test);
     }
-=======
-    throw new InvalidArgumentException(sprintf('The file "%s" is not in the project or a connected plugin’s lib directory.', $path));
-  }
-
-  /**
-   * Returns true if the supplied class uses the database.
-   * 
-   * @return boolean
-   */
-  protected function isDatabaseClass(ReflectionClass $r)
-  {
-    return
-      // propel
-      (class_exists('Propel') && ($r->isSubclassOf('BaseObject') || 'Peer' == substr($r->getName(), -4)))
-      ||
-      // doctrine
-      (class_exists('Doctrine') && ($r->isSubclassOf('Doctrine_Record') || $r->isSubclassOf('Doctrine_Table')))
-      ||
-      // either
-      $r->isSubclassOf('sfFormObject') || $r->isSubclassOf('sfFormFilter')
-    ;
->>>>>>> ba8708782531e237c25c55f198c7eacc5feee572
   }
 }
